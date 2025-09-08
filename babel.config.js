484cc9a57ef1254642da79080f4444d66133437f
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-09-08 19:06:11 +0530

    hh

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-worklets/plugin',
-      'expo-router/babel',
-    ],
+    plugins: ['react-native-worklets/plugin'],
   };
 };
 
